e905447ecf2bcca53a9a9f15ebc88fc93a1f69fd
Chris Neville <cmneville@henrico.k12.va.us>
2016-04-19 21:39:26 -0400

    Made the test class

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -11,14 +11,47 @@
  *
  */
 package map;
-import edu.princeton.cs.introcs.*;
+
+import java.io.File;
+import java.util.Scanner;
+
 /**
  *
- * @author 
+ * @author
  */
 public class PoliticalMap {
-    public static void main(String[] args){
-        
-        
+
+    public static void main(String[] args) {
+
+        Scanner scan;
+
+        try {
+            scan = new Scanner(new File("/data/AK.txt"));
+        } catch (Exception e) {
+            System.out.println("nope");
+            scan = null;
+        }
+
+        Coordinate min = new Coordinate(scan.nextDouble(), scan.nextDouble());
+        Coordinate max = new Coordinate(scan.nextDouble(), scan.nextDouble());
+        System.out.println(max);
+        int size = scan.nextInt();
+        Region region = new Region(min, max, size);
+        Subregion subregions;
+        for (int i = 0; i < size - 1; i++) {
+            String subTag = scan.nextLine();
+            String tag = scan.nextLine();
+            int subSize = scan.nextInt();
+
+            subregions = new Subregion(subSize);
+
+            for (int j = 0; j < subSize - 1; j++) {
+                subregions.addCoordinate(new Coordinate(scan.nextDouble(), scan.nextDouble()));
+            }
+            region.addSubregion(subregions);
+        }
+
+        region.draw();
+
     }
 }
